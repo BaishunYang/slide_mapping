6eca3683f0bf5a0a0972e6ab29ce686c06f3b5dd
BaishunYang <94889056+BaishunYang@users.noreply.github.com>
2023-02-06 08:58:40 +0100

    Update remove_isolate_interactions.py

--- remove_isolate_interactions.py
+++ remove_isolate_interactions.py
@@ -6,12 +6,15 @@ Created on Sat May 22 12:03:42 2021
 """
 
 
+### this file was used to remove the interactions that are not pair occurs in the twisted bilayer system.
+
 import numpy as np
 
 f=open("inputucf_Jzs.dat", 'r')
 line_f=f.readlines()
 linemax=len(line_f)
 
+## remove the parameters not pair occur
 for i in range (linemax):
     for j in range(linemax):
         if float(line_f[j].split()[0])==float(line_f[i].split()[1]) and float(line_f[j].split()[1])==float(line_f[i].split()[0]):
